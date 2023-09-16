e402a662a8e0e67c8e94646d568210ddd9d07a20
Ricardo Branco <rbranco@suse.de>
2023-09-16 21:23:13 +0200

    Add Github Page

---
+++ creds.py
@@ -0,0 +1,25 @@
+"""
+Create creds.json
+"""
+
+import json
+import os
+
+CREDS = {
+    "bugzilla.suse.com": {
+        "api_key": os.environ["BUGZILLA_API_KEY"],
+        "user": os.environ["BUGZILLA_USER"],
+    },
+    "github.com": {"login_or_token": os.environ["BUGME_GITHUB_TOKEN"]},
+    "gitlab.com": {"private_token": os.environ["GITLAB_API_KEY"]},
+    "progress.opensuse.org": {
+        "key": os.environ["REDMINE_API_KEY"],
+        "username": os.environ["REDMINE_USER"],
+    },
+}
+
+
+if __name__ == "__main__":
+    os.umask(0o077)
+    with open("creds.json", "w", encoding="utf-8") as file:
+        file.write(json.dumps(CREDS))
