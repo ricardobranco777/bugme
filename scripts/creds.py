348b819918a00ffa4f71f8502b1811da10f0ebe7
Ricardo Branco <rbranco@suse.de>
2023-10-15 13:02:46 +0200

    Add Jira more persistent cookie

--- scripts/creds.py
+++ scripts/creds.py
@@ -12,7 +12,9 @@ CREDS = {
     "github.com": {"login_or_token": os.getenv("BUGME_GITHUB_TOKEN")},
     "gitlab.com": {"private_token": os.getenv("GITLAB_API_KEY")},
     "jira.suse.com": {
+        "username": os.getenv("JIRA_USER"),
         "cookies": {
+            "seraph.rememberme.cookie": os.getenv("JIRA_COOKIE"),
             "JIRASESSIONID": os.getenv("JIRA_SESSION_ID"),
             "atlassian.xsrf.token": os.getenv("JIRA_TOKEN"),
         }
