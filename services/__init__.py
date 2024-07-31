8f2e79edf3813ae624533f97436358878bb6daef
Ricardo Branco <rbranco@suse.de>
2024-07-31 16:57:21 +0200

    Use frozen dataclass

--- services/__init__.py
+++ services/__init__.py
@@ -70,7 +70,7 @@ def xgetitem(*keys: str | int) -> Callable[[Any], Any]:
     return lambda item: reduce(getitem, keys, item)
 
 
-@dataclass(kw_only=True)
+@dataclass(frozen=True, kw_only=True)
 class Issue:  # pylint: disable=too-many-instance-attributes
     """
     Issue class
